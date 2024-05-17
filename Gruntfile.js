1bc430d96e44209232bd8f234a28c2c1b61fb9ca
Rafael Almeida <dealmeida.silva@hotmail.com>
2024-05-17 15:49:21 -0300

    sorteador

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,8 +38,6 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-replace');
-    grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
